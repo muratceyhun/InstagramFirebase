7aff676892477bcf804b8ba5ba966f1067092860
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-08-04 19:31:20 +0300

    The basic commits for the beginning of the project.

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Murat Ceyhun Korpeoglu on 4.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
