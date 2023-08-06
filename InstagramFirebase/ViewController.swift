2d6f81246d10002c58acc996bd5df15c8dfe4446
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-08-06 12:53:57 +0300

    Creating Registration Screen

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -8,10 +8,101 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handleAddPhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.placeholder = "Email"
+        return textField
+    }()
+    
+    let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.placeholder = "Username"
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.placeholder = "Password"
+        return textField
+    }()
+    
+    let signupButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    @objc func handleSignUp() {
+        print("12112121221")
+    }
+        
+    
+    @objc fileprivate func handleAddPhoto() {
+        print("****")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(plusPhotoButton)
+        
+        plusPhotoButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+//        view.addSubview(emailTextField)
+//
+//        NSLayoutConstraint.activate([
+//            emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 16),
+//            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            emailTextField.heightAnchor.constraint(equalToConstant: 50),
+//            emailTextField.widthAnchor.constraint(equalToConstant: 300)
+//        ])
+
+        
+        let stackView: UIStackView = {
+           let stack = UIStackView(arrangedSubviews: [emailTextField, userNameTextField, passwordTextField, signupButton])
+            stack.distribution = .fillEqually
+            stack.axis = .vertical
+            stack.spacing = 10
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            return stack
+        }()
+        
+        
+        view.addSubview(stackView)
+        
+        stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 240).isActive = true
+        
     }
 
 
